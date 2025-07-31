5e87a7c076cfc539a66ed310d6913b0a6c13debe
SubahaSalman <subaha.salman@gmail.com>
2025-07-31 18:10:01 +0200

    circumference circle

---
+++ circumference of a circle.py
@@ -0,0 +1,7 @@
+radius = int(input("Enter the radius of the circle: "))
+
+def circumference_calculation():
+    circumference = 2 * radius * 3.14
+    print (circumference)
+
+circumference_calculation()
